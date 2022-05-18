5b9364b24c1d63cafe8ea96c89e4f0ef654f758c
Philippe Lefebvre <philippe.lefebvre@epita.fr>
2022-05-18 20:49:42 +0200

    fixed cross origin font issue

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
         <meta name="freddies home" content="a place for everyone" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
         <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Poppins:wght@100&display=swap" rel="stylesheet" />
       </Head>
 
